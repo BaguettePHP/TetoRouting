4e26e41535cdc6b12b079910d54a2f9d50653d3e
USAMI Kenta <tadsan@zonu.me>
2015-03-06 03:30:50 +0900

    Add @property-read tag

--- src/Action.php
+++ src/Action.php
@@ -8,6 +8,14 @@ namespace Teto\Routing;
  * @author     USAMI Kenta <tadsan@zonu.me>
  * @copyright  2015 USAMI Kenta
  * @license    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @property-read string[] $methods
+ * @property-read string[] $split_path
+ * @property-read array    $param_pos
+ * @property-read mixed    $value
+ * @property-read string   $extension
+ * @property-read boolean  $is_wildcard
+ * @property-read string[] $available_extensions
  */
 class Action
 {
