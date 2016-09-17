c3266c850f6b1acd5ce83521c75e802dd39f32cc
USAMI Kenta <tadsan@zonu.me>
2016-09-17 13:40:11 +0900

    Add docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Teto\\Routing\\Action"],["c","Teto\\Routing\\CommonPrefixTrieRouter"],["c","Teto\\Routing\\NotFoundAction"],["c","Teto\\Routing\\Router"]];
